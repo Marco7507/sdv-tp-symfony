13384792f0f8df3274ac51bf3f653692dfa4d63b
Marc-Antoine Leonelli <marc-antoine.leonelli@littleworker.fr>
2025-04-10 13:37:16 +0200

    ✨ Add login

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Application\Auth\CreateUserUseCase;
+use App\Application\Auth\GetTokenUseCase;
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class AuthController extends AbstractController
 {
-    function __construct(private CreateUserUseCase $createUserUseCase)
+    function __construct(
+        private CreateUserUseCase $createUserUseCase,
+        private GetTokenUseCase   $getTokenUseCase
+    )
     {
-
     }
 
     #[Route('/register', name: 'register', methods: ['POST'])]
@@ -33,5 +39,22 @@ final class AuthController extends AbstractController
         } catch (\Exception $e) {
             return $this->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    #[Route('/login', name: 'login', methods: ['POST'])]
+    public function login(#[CurrentUser] ?User $user): Response
+    {
+        if ($user === null) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $this->getTokenUseCase->execute($user);
+
+        return $this->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
 }
